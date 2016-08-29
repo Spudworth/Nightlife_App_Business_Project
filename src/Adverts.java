2a79d6abe741f03405cc90f109d48470ca996623
Sean Healy <Sean.Healy@students.ittralee.ie>
2016-08-29 16:39:23 +0100

    3rd commit: Comment by Sean Healy: Added getters and setters for each variable as well as a multi argument constructor.

--- src/Adverts.java
+++ src/Adverts.java
@@ -4,10 +4,57 @@
 import java.util.*;
 public class Adverts {
 
+
+
     private int advertID;
     private String title;
     private String [] description;
     private Date startDate;
     private Date endDate;
+
+
+    public Adverts(String title, String[] description, Date startDate, Date endDate){
+
+        setTitle(title);
+        setDescription(description);
+        setStartDate(startDate);
+        setEndDate(endDate);
+    }
+
+    public int getAdvertID() {
+        return advertID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getDescription() {
+        return description;
+    }
+
+    public void setDescription(String[] description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 
  }
