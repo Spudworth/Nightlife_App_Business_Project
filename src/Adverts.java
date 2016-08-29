589fc92eff868c344c93a07ea718a38820197dbf
Sean Healy <Sean.Healy@students.ittralee.ie>
2016-08-29 16:25:40 +0100

    2nd commit: Comment by Sean Healy: Added variables advertID, title, description, startDate, endDate to the Adverts class. This commit is also to test connections with GitHub.

--- src/Adverts.java
+++ src/Adverts.java
@@ -1,5 +1,13 @@
 /**
  * Created by X99-A on 29-Aug-16.
  */
+import java.util.*;
 public class Adverts {
-}
+
+    private int advertID;
+    private String title;
+    private String [] description;
+    private Date startDate;
+    private Date endDate;
+
+ }
