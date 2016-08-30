ed69f1fa2e3518c3694f20bc9d97aae7f1334a4c
Sean Healy <Sean.Healy@students.ittralee.ie>
2016-08-30 17:12:20 +0100

    15th commit: Comment by Sean Healy: Added a new class called "BusinessApplicationDriver". This class has the main method of the program and will be the testing of the new GUI happens.

---
+++ src/BusinessApplicationDriver.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Sean Healy on 30-Aug-16.
+ */
+public class BusinessApplicationDriver {
+    static BusinessGUI mainGUI;
+    public static void main(String args []){
+        mainGUI = new BusinessGUI();
+        mainGUI.setVisible(true);
+
+    }
+}
